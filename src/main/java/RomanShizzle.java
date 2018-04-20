3e75f99af80dd7f098666e7f06a243540b90c7bb
Matthew Freake <matthew.freake@masabi.com>
2018-04-20 12:50:41 +0100

    Only pass in offset

--- src/main/java/RomanShizzle.java
+++ src/main/java/RomanShizzle.java
@@ -12,20 +12,20 @@ public class RomanShizzle {
     public static String of(int number) {
         StringBuffer result = new StringBuffer();
 
-        number = solveBaseNumbers(number, result, THINGS.get(2), THINGS.get(1), THINGS.get(0), 1000);
-        number = solveBaseNumbers(number, result, THINGS.get(4), THINGS.get(3), THINGS.get(2), 100);
-        number = solveBaseNumbers(number, result, THINGS.get(6), THINGS.get(5), THINGS.get(4), 10);
-        solveBaseNumbers(number, result, THINGS.get(8), THINGS.get(7), THINGS.get(6), 1);
+        number = solveBaseNumbers(number, result, 2, 1000);
+        number = solveBaseNumbers(number, result, 4, 100);
+        number = solveBaseNumbers(number, result, 6, 10);
+        solveBaseNumbers(number, result, 8, 1);
 
         return result.toString();
     }
 
-    private static int solveBaseNumbers(int number, StringBuffer result, String i, String v, String x, int multiplier) {
+    private static int solveBaseNumbers(int number, StringBuffer result, int i, int multiplier) {
 
-        number = consumeNumber(number, result, 9 * multiplier, i + x);
-        number = consumeNumber(number, result, 5 * multiplier, v);
-        number = consumeNumber(number, result, 4 * multiplier, i + v);
-        number = consumeNumber(number, result, 1 * multiplier, i);
+        number = consumeNumber(number, result, 9 * multiplier, THINGS.get(i) + THINGS.get(i-2));
+        number = consumeNumber(number, result, 5 * multiplier, THINGS.get(i-1));
+        number = consumeNumber(number, result, 4 * multiplier, THINGS.get(i) + THINGS.get(i-1));
+        number = consumeNumber(number, result, 1 * multiplier, THINGS.get(i));
 
         return number;
     }
